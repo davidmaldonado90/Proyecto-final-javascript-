040380721ab8307e8b8d8a459ae6c984bf6ac002
davidmaldonado <daviscarpm@gmail.com>
2022-06-30 21:18:33 -0300

    trabajando en js

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,6 @@
 //array carrito
 let carritoCompra = [];
 
-//array productos
-let stockProductos = [];
-
 //variables
 const contenedorProductos = document.getElementById("productos");
 const contenedorCarrito = document.getElementById("info");
@@ -11,96 +8,15 @@ const contadorCarrito = document.getElementById("contador");
 const precioTotal = document.getElementById("total");
 const boton = document.getElementById("fin");
 
-//constructor de producto
-
-class producto {
-  constructor(id, nombre, tipo, precio, imagen, cantidad) {
-    this.id = id;
-    this.tipo = tipo;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.imagen = imagen;
-    this.cantidad = cantidad;
-  }
-}
-
-stockProductos.push(
-  new producto(
-    0,
-    "Filet de merluza",
-    "Fresco",
-    750,
-    "../imagenes/FiletMerluza.jpg",
-    1
-  )
-);
-stockProductos.push(
-  new producto(
-    1,
-    "Filet de Pez Gallo",
-    "Fresco",
-    800,
-    "../imagenes/PezGallo.jpg",
-    1
-  )
-);
-stockProductos.push(
-  new producto(
-    2,
-    "Filet de Salmon Rosado",
-    "Fresco",
-    2800,
-    "../imagenes/salmon-salvaje.jpg",
-    1
-  )
-);
-stockProductos.push(
-  new producto(
-    3,
-    "Filet de Abadejo",
-    "Fresco",
-    1100,
-    "../imagenes/Abadejo.jpg",
-    1
-  )
-);
-stockProductos.push(
-  new producto(
-    4,
-    "Mejillon Media valva",
-    "Congelado",
-    750,
-    "../imagenes/MediaValva.jpg",
-    1
-  )
-);
-stockProductos.push(
-  new producto(
-    5,
-    "Tubo de Calamar",
-    "Congelado",
-    750,
-    "../imagenes/TuboCalamar.jpg",
-    1
-  )
-);
-
-// stockProductos.push(new producto(6, "Filet de merluza", Fresco, 750, "../imagenes/FiletMerluza.jpg", 1));
-// stockProductos.push(new producto(7, "Filet de merluza", Fresco, 750, "../imagenes/FiletMerluza.jpg", 1));
-// stockProductos.push(new producto(8, "Filet de merluza", Fresco, 750, "../imagenes/FiletMerluza.jpg", 1));
-// stockProductos.push(new producto(9, "Filet de merluza", Fresco, 750, "../imagenes/FiletMerluza.jpg", 1));
-// stockProductos.push(new producto(10, "Filet de merluza", Fresco, 750, "../imagenes/FiletMerluza.jpg", 1));
-
 const mostrarProductos = () => {
   stockProductos.forEach((el) => {
     let div = document.createElement("div");
-    div.innerHTML = `<div id="card" class="fresco">                     
+    div.innerHTML = `<div id="card" class="fresco">
                           <img src="${el.imagen}"/>
                           <h2>${el.nombre}</h2>
                           <p>$${el.precio}</p>
-                          <a id=boton${el.id}>Agregar al Carrito<i class="fa-solid fa-cart-plus"></i></a>                         
+                          <a id=boton${el.id}>Agregar al Carrito<i class="fa-solid fa-cart-plus"></i></a>
                     </div>`;
-
     contenedorProductos.appendChild(div);
     let btnCompra = document.getElementById(`boton${el.id}`);
     //console.log(btnCompra);
@@ -112,12 +28,18 @@ const mostrarProductos = () => {
 
 mostrarProductos();
 
+const carritoVacio = () => {
+  let vacio = document.getElementById("precioProducto");
+  if (carritoCompra == "") {
+    vacio.innerHTML = `<p>El carrito esta vacio</p>`;
+  }
+};
+
 const agregarCarrito = (id) => {
   let productoAñadir = stockProductos.find((item) => item.id === parseInt(id));
   carritoCompra.push(productoAñadir);
   mostrarCarrito(productoAñadir);
   actualizarCarrito();
-
   localStorage.setItem("carrito", JSON.stringify(carritoCompra));
 };
 
@@ -129,7 +51,6 @@ const mostrarCarrito = (productoAñadir) => {
                     <img src="${productoAñadir.imagen}"/>
                     <button id="eliminar${productoAñadir.id}" class="btn-eliminar"><i class="fa-solid fa-trash-can"></i></button>`;
   contenedorCarrito.appendChild(div);
-
   let btnEliminar = document.getElementById(`eliminar${productoAñadir.id}`);
   btnEliminar.addEventListener("click", () => {
     btnEliminar.parentElement.remove();
@@ -141,7 +62,6 @@ const mostrarCarrito = (productoAñadir) => {
 
 const actualizarCarrito = () => {
   contadorCarrito.innerText = carritoCompra.length;
-  precioTotal.innerText = carritoCompra.reduce((acc, el) => acc + el.precio, 0);
 };
 
 const recupero = () => {
@@ -188,7 +108,8 @@ const finalizar = () => {
       result.isConfirmed &&
         Swal.fire(
           "Gracias por su compra!",
-          "El Total a Abonar es: ",
+          "El Total a Abonar es: $" +
+            carritoCompra.reduce((acc, el) => acc + el.precio, 0),
           "success"
         );
     });
@@ -196,70 +117,3 @@ const finalizar = () => {
 };
 
 finalizar();
-// //funcion para comenzar la compra
-
-// let total = 0;
-
-// const carrito = () => {
-//   let otroProducto;
-//   let producto;
-//   let cantidad = 0;
-//   let precio = 0;
-//   do {
-//     producto = prompt("elegi el producto deseado");
-//     cantidad = parseInt(prompt("ingresa la cantidad"));
-
-//     switch (producto) {
-//       case misProductos[0].nombre:
-//         precio = misProductos[0].precio;
-//         break;
-//       case misProductos[1].nombre:
-//         precio = misProductos[1].precio;
-//         break;
-//       case misProductos[2].nombre:
-//         precio = misProductos[2].precio;
-//         break;
-//       default:
-//         alert("el producto no existe");
-//     }
-
-//     total = total + precio * cantidad;
-//     otroProducto = confirm("¿desea agregar otro producto?");
-//   } while (otroProducto);
-// };
-
-// // funcion para descuento
-
-// const descuento = (total) => {
-//   if (total >= 3000) {
-//     total = total * 0.9;
-//     alert("tenes un descuento del 10%");
-//   }
-//   return total;
-// };
-
-// //cobro de envio
-
-// const cobroEnvio = (total) => {
-//   let confirmacion = confirm("¿te lo enviamos a domicilio?");
-//   if (confirmacion && total >= 1500) {
-//     alert("tenes envio gratis. El total de tu compra es: " + total);
-//   } else if (confirmacion && total < 1500) {
-//     total = total + 300;
-//     alert("el total de la compra es: " + total);
-//   } else {
-//     alert("el total de la compra es: " + total);
-//   }
-//   return total;
-// };
-
-// //finalizar compra
-
-// const Compra = () => {
-//   ordenarLista();
-//   carrito();
-//   cobroEnvio(descuento(total));
-// };
-
-// Compra();
-// alert("gracias por su visita");
---
+++ javascript/stock.js
@@ -0,0 +1,82 @@
+//array productos
+let stockProductos = [];
+
+//constructor de producto
+
+class producto {
+  constructor(id, nombre, tipo, precio, imagen, cantidad) {
+    this.id = id;
+    this.tipo = tipo;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.imagen = imagen;
+    this.cantidad = cantidad;
+  }
+}
+
+stockProductos.push(
+  new producto(
+    1,
+    "Filet de merluza",
+    "Fresco",
+    750,
+    "../imagenes/FiletMerluza.jpg",
+    1
+  )
+);
+stockProductos.push(
+  new producto(
+    2,
+    "Filet de Pez Gallo",
+    "Fresco",
+    800,
+    "../imagenes/PezGallo.jpg",
+    1
+  )
+);
+stockProductos.push(
+  new producto(
+    3,
+    "Filet de Salmon Rosado",
+    "Fresco",
+    2800,
+    "../imagenes/salmon-salvaje.jpg",
+    1
+  )
+);
+stockProductos.push(
+  new producto(
+    4,
+    "Filet de Abadejo",
+    "Fresco",
+    1100,
+    "../imagenes/Abadejo.jpg",
+    1
+  )
+);
+stockProductos.push(
+  new producto(
+    5,
+    "Mejillon Media valva",
+    "Congelado",
+    750,
+    "../imagenes/MediaValva.jpg",
+    1
+  )
+);
+stockProductos.push(
+  new producto(
+    6,
+    "Tubo de Calamar",
+    "Congelado",
+    750,
+    "../imagenes/TuboCalamar.jpg",
+    1
+  )
+);
+
+// stockProductos.push(new producto(6, "Filet de merluza", Fresco, 750, "../imagenes/FiletMerluza.jpg", 1));
+// stockProductos.push(new producto(7, "Filet de merluza", Fresco, 750, "../imagenes/FiletMerluza.jpg", 1));
+// stockProductos.push(new producto(8, "Filet de merluza", Fresco, 750, "../imagenes/FiletMerluza.jpg", 1));
+// stockProductos.push(new producto(9, "Filet de merluza", Fresco, 750, "../imagenes/FiletMerluza.jpg", 1));
+// stockProductos.push(new producto(10, "Filet de merluza", Fresco, 750, "../imagenes/FiletMerluza.jpg", 1));
